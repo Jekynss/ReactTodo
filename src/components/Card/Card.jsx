416c2a6840aa16f1ba612d6a3ed8700cabbf73c5
Jekynss <Jekyns@mail.ru>
2020-08-04 18:52:38 +0300

    Fixed error

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 import styles from './Card.module.css'
 
@@ -10,9 +11,9 @@ export default function Card({id,name,age,gender,setShowModal}) {
 
     return (
         <div className={styles.card} id={id}>
-            <a href={`/dogs/${id}`}>
+            <Link to={`/dogs/${id}`}>
             <img className={styles.background_image} src={'https://placedog.net/250/250?random'} alt="Dog"/>
-            </a>
+            </Link>
             <div className={styles.info}>{name}</div>
             <div className={styles.info}>{age}</div>
             <div className={styles.info}>{gender}</div>
